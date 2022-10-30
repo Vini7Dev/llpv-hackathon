39c2893cda27b574da382ee65eae1206ef0f9244
Vinícius Gabriel <94920663+Vini7Dev@users.noreply.github.com>
2022-10-29 22:59:52 -0300

    feat: :art: implementation of signup steps

--- mobile/src/screens/Signup/index.tsx
+++ mobile/src/screens/Signup/index.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
-import { KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { ActivityIndicator, KeyboardAvoidingView, Platform } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
+import StepIndicator from 'react-native-step-indicator'
 
-import { Container, PageSubtitle, PageTitle, SubmitButtonArea, SubtitleImage } from './styles'
+import {
+  Container,
+  PageSubtitle,
+  PageTitle,
+  SubmitButtonArea,
+  SubtitleImage,
+  StepIndicatorContainer,
+  SuccessArea,
+} from './styles'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
 import emogiSubtitle from '../../assets/emogi-subtitle.png'
 
+const labels = ["Identificação","Acesso","Sucesso!"]
+const customStyles = {
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize:30,
+  separatorStrokeWidth: 2,
+  currentStepStrokeWidth: 3,
+  stepStrokeCurrentColor: '#2F6FED',
+  stepStrokeWidth: 3,
+  stepStrokeFinishedColor: '#2F6FED',
+  stepStrokeUnFinishedColor: '#aaaaaa',
+  separatorFinishedColor: '#2F6FED',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor: '#2F6FED',
+  stepIndicatorUnFinishedColor: '#ffffff',
+  stepIndicatorCurrentColor: '#ffffff',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 13,
+  stepIndicatorLabelCurrentColor: '#2F6FED',
+  stepIndicatorLabelFinishedColor: '#ffffff',
+  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+  labelColor: '#999999',
+  labelSize: 13,
+  currentStepLabelColor: '#2F6FED'
+}
+
 export const Signup: React.FC = () => {
+  const [currentPosition, setCurrentPosition] = useState(0)
+
   return (
     <Container>
       <KeyboardAvoidingView
@@ -17,22 +53,69 @@ export const Signup: React.FC = () => {
         enabled
       >
         <ScrollView keyboardShouldPersistTaps="handled">
-          <PageTitle>Falta pouco!</PageTitle>
-          <PageSubtitle>
-            Precisamos de alguns dados <SubtitleImage source={emogiSubtitle} width={32} height={32} />
-          </PageSubtitle>
+          <StepIndicatorContainer>
+            <StepIndicator
+              customStyles={customStyles}
+              currentPosition={currentPosition}
+              stepCount={3}
+              labels={labels}
+            />
+          </StepIndicatorContainer>
 
-          <Input name="socialReason" placeholder="Razão Social" icon="award" />
-          <Input name="cnpj" placeholder="CNPJ" icon="briefcase" />
-          <Input name="whatsapp" placeholder="Whatsapp" icon="phone" />
-          <Input name="email" placeholder="E-mail" icon="at-sign" />
-          <Input name="password" placeholder="Senha" icon="lock" />
-
-          <SubmitButtonArea>
-            <Button text="Cadastrar-se" />
-          </SubmitButtonArea>
+          { currentPosition === 0 && <Step1 onPressToContinue={setCurrentPosition} /> }
+          { currentPosition === 1 && <Step2 onPressToContinue={setCurrentPosition} /> }
+          { currentPosition === 2 && <Step3 /> }
         </ScrollView>
       </KeyboardAvoidingView>
     </Container>
+  )
+}
+
+const Step1: React.FC<{ onPressToContinue(step: number): void}> = ({
+  onPressToContinue,
+}) => {
+  return (
+    <>
+      <PageTitle>Falta pouco!</PageTitle>
+      <PageSubtitle>
+        Precisamos de alguns dados <SubtitleImage source={emogiSubtitle} width={32} height={32} />
+      </PageSubtitle>
+
+      <Input name="socialReason" placeholder="Razão Social" icon="award" />
+      <Input name="cnpj" placeholder="CNPJ" icon="briefcase" />
+      <Input name="whatsapp" placeholder="Whatsapp" icon="phone" />
+
+      <SubmitButtonArea>
+        <Button text="Próximo" onPress={() => onPressToContinue(1)} />
+      </SubmitButtonArea>
+    </>
+  )
+}
+
+const Step2: React.FC<{ onPressToContinue(step: number): void}> = ({
+  onPressToContinue,
+}) => {
+  return (
+    <>
+      <PageTitle>Última etapa!</PageTitle>
+      <PageSubtitle>
+        Para finalizar, insira seus dados de acesso
+      </PageSubtitle>
+
+      <Input name="email" placeholder="E-mail" icon="at-sign" />
+      <Input name="password" placeholder="Senha" icon="lock" />
+
+      <SubmitButtonArea>
+        <Button text="Cadastrar-se" onPress={() => onPressToContinue(2)} />
+      </SubmitButtonArea>
+    </>
+  )
+}
+
+const Step3: React.FC = () => {
+  return (
+    <SuccessArea>
+      <ActivityIndicator color="#2F6FED" size={52} />
+    </SuccessArea>
   )
 }
--- mobile/src/screens/Signup/styles.ts
+++ mobile/src/screens/Signup/styles.ts
@@ -5,7 +5,10 @@ export const Container = styled.View`
   height: 100%;
   justify-content: center;
   padding: 0 25px;
+`
 
+export const StepIndicatorContainer = styled.View`
+  margin-top: 100px;
 `
 
 export const PageTitle = styled.Text`
@@ -13,7 +16,7 @@ export const PageTitle = styled.Text`
   font-size: 24px;
   line-height: 29px;
   color: #2F6FED;
-  margin: 100px 0 50px;
+  margin: 40px 0 50px;
 `
 
 export const PageSubtitle = styled.Text`
@@ -29,4 +32,10 @@ export const SubtitleImage = styled.Image``
 
 export const SubmitButtonArea = styled.View`
   margin-bottom: 100px;
+`
+
+export const SuccessArea = styled.View`
+  width: 100%;
+  height: 100%;
+  margin-top: 150px;
 `
